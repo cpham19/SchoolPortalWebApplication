664731a692d4d584554869161c05fee3b252733d
Calvin Pham <calvinthepham@gmail.com>
2019-09-18 21:48:58 -0700

    Deploy in Heroku

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store'
 
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:8081`,
+        baseURL: `https://csula-center.herokuapp.com/`,
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
