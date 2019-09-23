a2842b5c95ffe098af7468952e891381c823ee6c
Calvin Pham <calvinthepham@gmail.com>
2019-09-23 12:12:49 -0700

    Deploy in Heroku

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import store from '@/store/store'
 
-//https://csula-center.herokuapp.com/
+//https://goschoolportal.herokuapp.com/
 //http://localhost:8081
 
 export default () => {
     return axios.create({
-        baseURL: `https://csula-center.herokuapp.com/`,
+        baseURL: `https://goschoolportal.herokuapp.com/`,
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
